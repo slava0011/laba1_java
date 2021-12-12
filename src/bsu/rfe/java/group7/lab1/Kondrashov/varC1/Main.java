ba67014b33a6a59a4b39708c4237b876d92ea29f
slava0011 <mr.slawa2003@mail.ru>
2021-12-12 19:31:32 +0300

    added try catch for main class

---
+++ src/bsu/rfe/java/group7/lab1/Kondrashov/varC1/Main.java
@@ -0,0 +1,68 @@
+package bsu.rfe.java.group7.lab1.Kondrashov.varC1;
+
+import java.lang.reflect.*;
+import java.util.Comparator;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
+        Food[] breakfast = new Food[20];
+
+        boolean sort_flag = false;
+        boolean calories_flag = false;
+
+        int count_breakfast = 0;
+        int itemsSoFar = 0;
+        for (String arg : args) {
+            String[] partsOfArguments = arg.split("/");
+            if (partsOfArguments[0].equals("-sort")) {
+                sort_flag = true;
+                continue;
+            }
+            if (partsOfArguments[0].equals("-calories")) {
+                calories_flag = true;
+                continue;
+            }
+
+            String[] parts = arg.split("/");
+            if (parts[0].equals("Cheese")) {
+                breakfast[itemsSoFar] = new Cheese();
+            } else if (parts[0].equals("Meat")) {
+                breakfast[itemsSoFar] = new Meat(parts[1]);
+                //System.out.println(parts[1]);
+            } else if (parts[0].equals("Sandwich")) {
+                breakfast[itemsSoFar] = new Sandwich(parts[1], parts[2]);
+            }
+            itemsSoFar++;
+            try {
+                Class myClass = Class.forName("bsu.rfe.java.group7.lab1." + partsOfArguments[0]);
+
+                if (partsOfArguments.length == 1) {
+
+                    Constructor constructor = myClass.getConstructor();
+                    breakfast[count_breakfast] = (Food) constructor.newInstance();
+                } else if (partsOfArguments.length == 2) {
+
+                    Constructor constructor = myClass.getConstructor(String.class);
+                    breakfast[count_breakfast] = (Food) constructor.newInstance(partsOfArguments[1]);
+                }
+                if (partsOfArguments.length == 3) {
+
+                    Constructor constructor = myClass.getConstructor(String.class, String.class);
+                    breakfast[count_breakfast] = (Food) constructor.newInstance(partsOfArguments[1], partsOfArguments[2]);
+                }
+            } catch (ClassNotFoundException e) {
+                System.out.println("(" + count_breakfast + ")This product can't be eaten");
+                continue;
+            } catch (NoSuchMethodException e) {
+                System.out.println("There are no some method");
+                continue;
+            } catch (InstantiationException e) {
+                continue;
+            }
+
+            breakfast[count_breakfast].consume();
+            count_breakfast += 1;
+        }
+    }
+}
