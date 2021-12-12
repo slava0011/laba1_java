312b9fb7f9dd6a9a8aec9b3b8373c57c3b92e992
slava0011 <mr.slawa2003@mail.ru>
2021-12-12 19:23:38 +0300

    added consume, calculateCalories, equalstoString metods

--- src/bsu/rfe/java/group7/lab1/Kondrashov/varC1/Meat.java
+++ src/bsu/rfe/java/group7/lab1/Kondrashov/varC1/Meat.java
@@ -12,5 +12,34 @@ public class Meat extends Food {
         this.animal = animal;
     }
 
+    public void consume(){
+        System.out.println(this + " съеден");
+    }
 
+    public int calculateCalories(){
+        switch (animal){
+            case "pig":  return 270;
+            case "cow": return 300;
+            case "dog": return 450;
+            default:
+                animal = "some animal";
+                return 100;
+        }
+    }
+
+    public boolean equals(Object arg0){
+        if(super.equals(arg0)){
+            if(arg0 instanceof Meat){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public String toString(){
+        return super.toString();
+    }
 }
